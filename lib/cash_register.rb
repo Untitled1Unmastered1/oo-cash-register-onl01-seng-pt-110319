b3fdd9e04b75db1a26eb83f5993ba14de4f3537f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-22 23:23:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
+class CashRegister 
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @cart = []
+  end
+  
+  def add_item(title,price,count = 1)
+    item = {}
+    item[:title] = title 
+    item[:price] = price 
+    item[:count] = count 
+    @cart << item 
+    @total += price * count 
+  end 
+  
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    else 
+    @total -= @total * @discount / 100
+    return "After the discount, the total comes to $#{@total}."
+  end
+end 
+
+def items
+    item_names = []
+    @cart.each do | item_info |
+      for qty in 1..item_info[:quantity] 
+        item_names << item_info[:name]
+      end 
+    end 
+    item_names
+  end 
+end 
+
+  
+
+
+
 
