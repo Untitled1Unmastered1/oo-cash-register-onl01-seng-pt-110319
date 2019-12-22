598987bbdabea600737ffee66b78ccb30f295ab7
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-22 23:56:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,49 +1,48 @@
-require 'pry'
-class CashRegister 
-  attr_accessor :total, :discount, :last_transaction
+# require 'pry'
+# class CashRegister 
+#   attr_accessor :total, :discount, :last_transaction
   
-  def initialize(discount = 0)
-    @total = 0 
-    @discount = discount
-    @cart = []
-  end
+#   def initialize(discount = 0)
+#     @total = 0 
+#     @discount = discount
+#     @cart = []
+#   end
   
-  def add_item(title,price,count = 1)
-    item = {}
-    item[:title] = title 
-    item[:price] = price 
-    item[:count] = count 
-    @cart << item 
-    @total += price * count 
-  end 
-  self.last_transaction = price * count 
-end 
+#   def add_item(title,price,count = 1)
+#     item = {}
+#     item[:title] = title 
+#     item[:price] = price 
+#     item[:count] = count 
+#     @cart << item 
+#     @total += price * count 
+#   end 
+#   self.last_transaction = price * count 
+# end 
 
   
-  def apply_discount
-    if @discount == 0
-      return "There is no discount to apply."
-    else 
-    @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
-  end
-end 
+#   def apply_discount
+#     if @discount == 0
+#       return "There is no discount to apply."
+#     else 
+#     @total -= @total * @discount / 100
+#     return "After the discount, the total comes to $#{@total}."
+#   end
+# end 
 
-  def items
-    item_names = []
-    @cart.each do | item_info |
-      for qty in 1..item_info[:count] 
-        item_names << item_info[:title]
-      end 
-    end 
-    item_names
-  end 
+#   def items
+#     item_names = []
+#     @cart.each do | item_info |
+#       for qty in 1..item_info[:count] 
+#         item_names << item_info[:title]
+#       end 
+#     end 
+#     item_names
+#   end 
   
-  def void_last_transaction
-    @total = self.total - self.last_transaction
-  end 
-end 
-end 
+#   def void_last_transaction
+#     @total = self.total - self.last_transaction
+#   end 
+# end 
 
   
 
