6f42927044b3b128ade5a4f7bcf3747270026168
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-23 00:54:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -65,7 +65,9 @@ class CashRegister
   
     def void_last_transaction	
       self.total -= self.last_transaction[1] * self.last_transaction[2]
-      self.last_transaction[2].times do	
+      self.last_transaction[2].times do
+        self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+        
       
 
 
