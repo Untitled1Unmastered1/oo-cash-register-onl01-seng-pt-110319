882ee3c510a47569b2d517de3be3e6917a91472f
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-23 00:06:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister 
-  attr_accessor :total, :discount, :last_transaction
+  attr_accessor :total, :discount, :items, :last_transaction
   
   def initialize(discount = 0)
     @total = 0 
