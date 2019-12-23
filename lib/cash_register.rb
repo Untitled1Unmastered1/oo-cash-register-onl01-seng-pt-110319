2d6113144dbe5abc8a08a9d3c8163f8d68dd17e5
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-23 00:51:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -56,6 +56,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
     quantity.times { self.items << title }
+    self.last_transaction = [title, price, quantity]
     
 
 
